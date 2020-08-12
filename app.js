f945bc0d92806bcf37d477a82864a293cd69121b
xnslx <xnslx89@gmail.com>
2020-08-11 23:41:56 -0500

    keep learning

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('in the middleware');
+    next();
+})
+
+app.use((req, res, next) => {
+    console.log('in another middleware')
+    res.send('<h1>Hello from Express!</h1>')
+})
+
+const server = http.createServer(app);
+
+server.listen(3000)
--- first-app.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-
-fs.writeFileSync('hello.txt', 'hello from node.js')
