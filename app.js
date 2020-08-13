30ba024105f83ec7c53c876b15acf1a50a2dcbe6
xnslx <xnslx89@gmail.com>
2020-08-12 23:44:16 -0500

    second commit

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('in the middleware');
-    next();
-})
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    console.log('in another middleware')
-    res.send('<h1>Hello from Express!</h1>')
+    res.status(404).send('<h1>Page not found!</h1>')
 })
 
-const server = http.createServer(app);
-
-server.listen(3000)
+app.listen(3000)
