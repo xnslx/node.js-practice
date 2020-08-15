3e68c6a1794dac932b010516ea6a9dc9edbd0bab
xnslx <xnslx89@gmail.com>
2020-08-14 23:04:32 -0500

    serving css files statically

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
