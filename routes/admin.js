a71ac5f389ff1843d3a20589a8e78db6a2078c51
xnslx <xnslx89@gmail.com>
2020-08-17 23:58:38 -0500

    refactor

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,13 @@ const products = [];
 
 router.get('/add-product', (req, res, next) => {
     // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-    res.render('add-product', { pageTitle: 'Add Product' })
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    })
 })
 
 router.post('/add-product', (req, res, next) => {
