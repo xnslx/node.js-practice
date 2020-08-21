49c2161fda7662f25f7a6b4ee06628c072b10d87
xnslx <xnslx89@gmail.com>
2020-08-20 22:21:09 -0500

    keep working on the add product

--- models/product.js
+++ models/product.js
@@ -15,8 +15,11 @@ const getProductsFromFile = cb => {
 }
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
