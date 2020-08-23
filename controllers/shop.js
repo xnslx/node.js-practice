cd9d8e2917c787c3ffd4058530b87f62eec531c2
xnslx <xnslx89@gmail.com>
2020-08-22 23:02:40 -0500

    fix a routing bug

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,9 +14,13 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
     Product.findById(prodId, product => {
-        console.log(product)
+        // console.log(product)
+        res.render('shop/product-detail', {
+            product: product,
+            pageTitle: product.title,
+            path: '/products'
+        })
     })
-    res.redirect('/')
 }
 
 exports.getIndex = (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,6 @@
-const express = require('express');
-
 const path = require('path')
+
+const express = require('express');
 
 const shopController = require('../controllers/shop')
 
@@ -10,7 +10,7 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-router.get('/products/:productId')
+router.get('/products/:productId', shopController.getProduct)
 
 router.get('/cart', shopController.getCart)
 
