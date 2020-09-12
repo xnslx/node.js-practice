3eb6290206955ecf561ed11440d60e4199be48a6
xnslx <xnslx89@gmail.com>
2020-09-11 22:17:15 -0500

    edit product

--- controllers/admin.js
+++ controllers/admin.js
@@ -131,7 +131,7 @@ exports.postDeleteProduct = (req, res, next) => {
 // }
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
+    Product.find()
         .then(products => {
             res.render('admin/products', {
                 prods: products,
--- routes/admin.js
+++ routes/admin.js
@@ -9,11 +9,11 @@ const router = express.Router();
 
 router.get('/add-product', adminController.getAddProduct);
 
-// router.get('/products', adminController.getProducts)
+router.get('/products', adminController.getProducts)
 
 router.post('/add-product', adminController.postAddProduct);
 
-// router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 // router.post('/edit-product', adminController.postEditProduct);
 
