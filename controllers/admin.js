c98df5af98fbbfcdd0a6230cc5f00e55ef6827e8
xnslx <xnslx89@gmail.com>
2020-09-11 22:26:21 -0500

    delete product

--- controllers/admin.js
+++ controllers/admin.js
@@ -124,7 +124,8 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.deleteById(prodId)
+    // Product.deleteById(prodId)
+    Product.findByIdAndRemove(prodId)
         .then(() => {
             console.log('destroyed product')
             res.redirect('/admin/products')
