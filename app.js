9c465f3c8bd5bee5e443992e2a0b5691d3ef976c
xnslx <xnslx89@gmail.com>
2020-09-18 22:44:18 -0500

    commit

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set('views', 'views')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -35,6 +36,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
+app.use(authRoutes)
 
 app.use(errorController.get404)
 
---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const authController = require('../controllers/auth');
+
+const router = express.Router();
+
+
+
+router.get('/login', authController.getLogin)
+
+
+module.exports = router;
