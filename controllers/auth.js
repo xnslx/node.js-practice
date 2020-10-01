772038c32e1faf699b727f96ed92817b25dad6bf
xnslx <xnslx89@gmail.com>
2020-09-30 23:12:17 -0500

    add validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator/check')
 const User = require('../models/user');
 require('dotenv').config()
 const transporter = nodemailer.createTransport(sendgridTransport({
@@ -80,6 +81,15 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.array())
+        return res.status(422).render('auth/signup', {
+            path: '/signup',
+            pageTitle: 'Signup',
+            errorMessage: errors.array()[0].msg
+        })
+    }
     User.findOne({ email: email })
         .then(userDoc => {
             if (userDoc) {
