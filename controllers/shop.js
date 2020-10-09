a93e02a8caae8fd4554ef2657ee9a27c17c631d5
xnslx <xnslx89@gmail.com>
2020-10-08 23:54:07 -0500

    add pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,6 +5,8 @@ const PDFDocument = require('pdfkit');
 
 const Product = require('../models/product');
 const Order = require('../models/order');
+
+const ITEMS_PER_PAGE = 1;
 // const Cart = require('../models/cart');
 // const User = require('../models/user');
 // const { ObjectId } = require('mongodb');
@@ -22,13 +24,25 @@ const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
     // Product.fetchAll()
-    Product.find()
+    const page = +req.query.page || 1;
+    let totalItems;
+    Product.find().countDocuments().then(numProducts => {
+            totalItems = numProducts;
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE)
+        })
         .then(products => {
-            console.log(products)
             res.render('shop/product-list', {
                 prods: products,
-                pageTitle: 'All Products',
-                path: '/products'
+                pageTitle: 'Products',
+                path: '/products',
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
             })
         })
         .catch(err => {
@@ -74,12 +88,25 @@ exports.getProduct = (req, res, next) => {
 
 exports.getIndex = (req, res, next) => {
     // Product.fetchAll()
-    Product.find()
+    const page = +req.query.page || 1;
+    let totalItems;
+    Product.find().countDocuments().then(numProducts => {
+            totalItems = numProducts;
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE)
+        })
         .then(products => {
             res.render('shop/index', {
                 prods: products,
                 pageTitle: 'Shop',
-                path: '/'
+                path: '/',
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
             })
         })
         .catch(err => {
